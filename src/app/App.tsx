42003824242b29542a7685ea3595bf081a99520f
IBROHIM95 <shodibek.tokhirov95@gmail.com>
2023-11-11 19:14:19 +0900

    feat:checking react-router-dom

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,15 +1,59 @@
 import { Container, Stack, Box, Typography,Button } from '@mui/material';
 import React from 'react';
 import { RippleBadge } from './MaterialTheme/styled';
-
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Users from './components/users';
+import Dishes from './components/Dishes';
 
 
 function App() {
   return (
-   <Container  >
-     
-   </Container>
+    
+   <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/dishes">Dishes</Link>
+          </li>
+          <li>
+            <Link to="/users">Users</Link>
+          </li>
+        </ul>
+
+        <Switch>
+          <Route path="/dishes">
+            <Dishes/>
+          </Route>
+          <Route path="/users">
+            <Users/>
+          </Route>
+          <Route path="/">
+            <Container>
+              <Home />
+            </Container>
+            
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+    
+  
   );
 }
 
 export default App;
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+
+
